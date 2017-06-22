72c095331b4c51a714623b133d31ad83bef8959b
zql <zql3315@163.com>
2017-06-22 18:45:44 +0800

    review

--- base-manager/src/main/java/com/infosky/wechat/entity/po/WeiXinKeyword.java
+++ base-manager/src/main/java/com/infosky/wechat/entity/po/WeiXinKeyword.java
@@ -7,6 +7,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.Lob;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -33,7 +34,8 @@ public class WeiXinKeyword extends PO<String> {
     @Comment(value = "关键字")
     private String keyword;
 
-    @Column(name = "description", columnDefinition = "TEXT")
+    @Lob
+    @Column(name = "description")
     @Comment(value = "文字描述")
     private String description;
 
